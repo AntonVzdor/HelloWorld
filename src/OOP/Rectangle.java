404f8759b85b926c1c62738ab46cacf2dd75b90d
AntonVzdor <135133877+AntonVzdor@users.noreply.github.com>
2025-08-20 00:02:41 +0300

    ООП

--- src/OOP/Rectangle.java
+++ src/OOP/Rectangle.java
@@ -1,10 +1,18 @@
 package OOP;
 
+import java.util.Scanner;
+
 public class Rectangle {
     public static void main(String[] args) {
         RectangleParam rectangle = new RectangleParam();
 
-        rectangle.valuesRectangle(20, 13);
+        Scanner in = new Scanner(System.in);
+        System.out.println("length");
+        int len = in.nextInt();
+        System.out.println("width");
+        int wid = in.nextInt();
+
+        rectangle.valuesRectangle(len, wid);
         System.out.println(rectangle.square());
     }
 }
