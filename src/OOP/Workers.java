4e4ba75c0f1c3cff0c6202709f4c6968b8cd1f65
dernom <der_nom@mail.ru>
2025-10-14 15:50:34 +0300

    ООП(завершено)

--- src/OOP/Workers.java
+++ src/OOP/Workers.java
@@ -4,66 +4,23 @@ import java.util.ArrayList;
 
 public class Workers {
     public static void main(String[] args) {
-
-        Director director = new Director();
-        Programmer programmer = new Programmer();
-        Cook cook = new Cook();
-
-        ArrayList<WorkerInter> workers = new ArrayList<>();
-        workers.add(director);
-        workers.add(programmer);
-        workers.add(cook);
-
-        for (int i = 0; i < workers.size();) {
-            director.work();
-            programmer.work();
-            cook.work();
-            break;
-        }
-
-        for (int i = 0; i < workers.size();) {
-         cook.drive();
-         programmer.drive();
-            break;
-        }
+        Client client = new Client();
+        client.myOrder(new Waiter() {
+            @Override
+            public void BringOrder(String order) {
+                System.out.println("Приятного аппетита");
+            }
+        }, "Суши");
     }
 }
 
-interface WorkerInter{
-    void work();
+interface Waiter{
+    void BringOrder(String order);
 }
 
-interface Driver {
-    void drive();
-}
-
-class Director implements WorkerInter{
-    @Override
-    public void work() {
-        System.out.println("Директор принемает решения");
-    }
-}
-
-class Programmer implements WorkerInter, Driver{
-    @Override
-    public void work() {
-        System.out.println("Программист пишет код");
-    }
-
-    @Override
-    public void drive() {
-        System.out.println("Прогер еще и водит тачку");
-    }
-}
-
-class Cook implements WorkerInter, Driver{
-    @Override
-    public void work() {
-        System.out.println("Повар готовит для всех");
-    }
-
-    @Override
-    public void drive() {
-        System.out.println("Повар еще и водит тачку");
+class Client{
+    public void myOrder(Waiter waiter, String order){
+        System.out.println("Ваш заказ: " + order);
+        waiter.BringOrder(order);
     }
 }
