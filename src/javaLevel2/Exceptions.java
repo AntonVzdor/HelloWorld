d45882463d0a07ae00ad69a6ac8e753b8b7b0815
AntonVzdor <135133877+AntonVzdor@users.noreply.github.com>
2025-10-21 16:06:36 +0300

    JAVA LEVEL 2

---
+++ src/javaLevel2/Exceptions.java
@@ -0,0 +1,16 @@
+package javaLevel2;
+
+public class Exceptions {
+    public static void main(String[] args) {
+
+        String[] numbs = new String[]{"1","2","3","4","5"};
+
+        try {
+            System.out.println(numbs[6]);
+        } catch (ArrayIndexOutOfBoundsException s){
+            System.out.println("Ошибка " + s.getClass());
+        } catch (Exception e){
+            System.out.println("no");
+        }
+    }
+}
