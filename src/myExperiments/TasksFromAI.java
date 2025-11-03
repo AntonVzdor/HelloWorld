5670d7602e4da3b2915716dee50236273d78b3fb
AntonVzdor <135133877+AntonVzdor@users.noreply.github.com>
2025-11-03 17:10:03 +0300

    JAVA LEVEL 2

--- src/myExperiments/TasksFromAI.java
+++ src/myExperiments/TasksFromAI.java
@@ -2,20 +2,22 @@ package myExperiments;
 
 public class TasksFromAI {
     public static void main(String[] args) {
-        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] numbers = {10, 20, 30, 40, 50};
 
-        SumNum.sumNum(numbers);
+        CheckNumber.checkNumber(numbers);
     }
 }
 
-class SumNum{
-    public static void sumNum(int[] numbers){
+class CheckNumber{
+    public static void checkNumber(int[] numbers){
         int sum = 0;
-        for (int num: numbers){
-            if (num % 2 == 0){
-                sum += num;
-            }
+
+        for (int num: numbers) {
+            sum += num;
         }
-        System.out.printf("Сумма чётных чисел: %s", sum);
+
+        double sum1 = (double) sum / numbers.length;;
+
+        System.out.println(sum1);
     }
 }
