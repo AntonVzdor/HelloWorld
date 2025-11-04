0c5f2c6e25c05d9b566b46b583a3bb0df2a273a8
AntonVzdor <135133877+AntonVzdor@users.noreply.github.com>
2025-11-04 16:39:59 +0300

    JAVA LEVEL 2

--- src/myExperiments/TasksFromAI.java
+++ src/myExperiments/TasksFromAI.java
@@ -2,22 +2,16 @@ package myExperiments;
 
 public class TasksFromAI {
     public static void main(String[] args) {
-        int[] numbers = {10, 20, 30, 40, 50};
 
-        CheckNumber.checkNumber(numbers);
     }
 }
 
-class CheckNumber{
-    public static void checkNumber(int[] numbers){
-        int sum = 0;
+class Person{
+    static String name;
+    static int age;
 
-        for (int num: numbers) {
-            sum += num;
-        }
-
-        double sum1 = (double) sum / numbers.length;;
-
-        System.out.println(sum1);
+    public void findOlderThan(String name, int age){
+        Person.name = name;
+        Person.age = age;
     }
 }
