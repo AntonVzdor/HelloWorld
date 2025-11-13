f87af91c817ba6af9f11f6ba81521774b5f62063
AntonVzdor <135133877+AntonVzdor@users.noreply.github.com>
2025-11-13 12:55:14 +0300

    JAVA LEVEL 2

--- src/myExperiments/TasksFromAI.java
+++ src/myExperiments/TasksFromAI.java
@@ -1,17 +1,53 @@
 package myExperiments;
 
+import java.util.ArrayList;
+
 public class TasksFromAI {
     public static void main(String[] args) {
 
     }
 }
 
-class Person{
-    static String name;
-    static int age;
+class Book {
 
-    public void findOlderThan(String name, int age){
-        Person.name = name;
-        Person.age = age;
+    private int id;
+    private String title;
+    private String author;
+    private int age;
+    private String world;
+
+    Book(
+            int id,
+            String title,
+            String author,
+            int age,
+            String world
+    ) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.age = age;
+        this.world = world;
     }
+
+    public int getId(){
+        return id;
+    }
+
+    public static void showInfo(Book book) {
+        System.out.printf(
+                "Выбранная вами книга:\n" +
+                        " Название: %s\n" +
+                        " автор: %s\n" +
+                        " год написания: %s\n" +
+                        " мир книги:%s",
+                book.title, book.author, book.age, book.world
+        );
+    }
+}
+
+class Library{
+
+    ArrayList<Book> books = new ArrayList<>();
+
 }
