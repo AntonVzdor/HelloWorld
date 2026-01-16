b2e811df176fbddfba677b3f12f3ade45fb65657
dernom <der_nom@mail.ru>
2026-01-17 00:03:24 +0300

    JAVA ADVANCED LEVEL

---
+++ src/JavaAdvancedLevel/Calculator.java
@@ -0,0 +1,22 @@
+package JavaAdvancedLevel;
+
+public class Calculator {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Operations{
+    public int addition (int a, int b){
+        return a+b;
+    }
+    public int subtraction(int a, int b){
+        return a-b;
+    }
+    public int multiplication(int a, int b){
+        return a*b;
+    }
+    public int division(int a, int b){
+        return a/b;
+    }
+}
