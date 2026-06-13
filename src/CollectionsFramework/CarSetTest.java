0b76da2c83a794973826335f7a21c039faf986d2
dernom <der_nom@mail.ru>
2026-06-13 16:00:08 +0300

    LinkedList

--- src/CollectionsFramework/CarSetTest.java
+++ src/CollectionsFramework/CarSetTest.java
@@ -1,6 +1,7 @@
 package CollectionsFramework;
 
 import org.junit.Before;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.*;
 
@@ -11,6 +12,33 @@ public class CarSetTest {
     @Before
     public void setUp() throws Exception {
         //init
+        for (int i = 0; i < 100; i++) {
+            carSet.add(new CarArray("Brand" + i, i));
+        }
+    }
+
+    @Test
+    public void element3(){
+        assertEquals(100, carSet.size());
+        assertTrue(carSet.add(new CarArray("Brand",10)));
+        assertFalse(carSet.add(new CarArray("Brand",10)));
+        assertFalse(carSet.add(new CarArray("Brand",10)));
+        assertEquals(101, carSet.size());
+    }
+
+    @Test
+    public void removeSetClear(){
+        carSet.clear();
+        assertEquals(0, carSet.size());
+    }
+
+    @Test
+    public void removeSet(){
+       assertTrue(carSet.remove(new CarArray("Brand30",30)));
+        assertEquals(99, carSet.size());
+        assertFalse(carSet.remove(new CarArray("Brand30",30)));
+        assertEquals(99, carSet.size());
 
     }
+
 }
