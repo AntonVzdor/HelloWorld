3ce515d3635815cd1a64199edaeef7e583d3f96c
dernom <der_nom@mail.ru>
2026-08-05 17:20:39 +0300

    проект собака

--- src/myExperiments/Main.java
+++ src/myExperiments/Main.java
@@ -2,8 +2,6 @@ package myExperiments;
 
 public class Main {
     public static void main(String[] args) {
-        Book book = new Book(1,"Дюна","Фрэнк Герберт",1965, "научная-фантастика", false);
-        String newBook = book.toString();
-        System.out.println(newBook);
+
     }
 }
