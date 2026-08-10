c09eb80e90949a17074f617592b2af041eb388d0
AntonVzdor <135133877+AntonVzdor@users.noreply.github.com>
2026-08-10 15:55:26 +0300

    Многопоточность Часть 2. ДЗ

---
+++ src/Multithreading/MultithreadingTest.java
@@ -0,0 +1,69 @@
+package Multithreading;
+
+class MultithreadingTest {
+
+    private static final int SIZE = 1000000;
+    private static final int HALF = SIZE/2;
+
+    public static void main(String[] args) {
+        withConcurrency();
+        withoutConcurrency();
+    }
+
+    private static void withConcurrency(){
+        float[] array = new float[SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            array[i] = 1f;
+        }
+        long before = System.currentTimeMillis();
+        for (int i = 0; i < SIZE; i++) {
+            float f = (float) i;
+            array[i] = (float) (array[i] * Math.sin(0.2f + f / 5) * Math.cos(0.2f + f / 5) * Math.cos(0.4f + f / 2));
+        }
+        long after = System.currentTimeMillis();
+        System.out.println(after - before);
+    }
+
+    private static  void withoutConcurrency(){
+        float[] array = new float[SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            array[i] = 1f;
+        }
+        long before = System.currentTimeMillis();
+        float[] firstHalf = new float[HALF];
+        float[] secondHalf = new float[HALF];
+        System.arraycopy(array, 0, firstHalf, 0, HALF);
+        System.arraycopy(array, HALF, secondHalf, 0, HALF);
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < HALF; i++) {
+                    float f = (float) i;
+                    array[i] = (float) (array[i] * Math.sin(0.2f + f / 5) * Math.cos(0.2f + f / 5) * Math.cos(0.4f + f / 2));
+                }
+            }
+        });
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < HALF; i++) {
+                    float f = (float) i;
+                    array[i] = (float) (array[i] * Math.sin(0.2f + f / 5) * Math.cos(0.2f + f / 5) * Math.cos(0.4f + f / 2));
+                }
+            }
+        });
+        t1.start();
+        t2.start();
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.arraycopy(firstHalf, 0, array, 0, HALF);
+        System.arraycopy(secondHalf, 0, array, HALF, HALF);
+        long after = System.currentTimeMillis();
+        System.out.println(after - before);
+    }
+
+}
