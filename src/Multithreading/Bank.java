dfcaf5d5cdd3f9a1344f378bd78d06ea3f12f7c2
AntonVzdor <135133877+AntonVzdor@users.noreply.github.com>
2026-08-17 13:32:58 +0300

    Многопоточность Часть 4. Синхронизация методов и мониторы

---
+++ src/Multithreading/Bank.java
@@ -0,0 +1,18 @@
+package Multithreading;
+
+public class Bank {
+    public int money = 10_000;
+
+    public void takeMoney(String name, int sum){
+        System.out.println(name + " подошел к банкомату");
+
+    }
+
+    public void takeMoney2(String name, int sum){
+
+    }
+
+    public void takeMoney3(String name, int sum){
+
+    }
+}
