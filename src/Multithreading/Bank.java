d597c24d45f87b3f6d9d3f8885bd2a49718844e3
AntonVzdor <135133877+AntonVzdor@users.noreply.github.com>
2026-08-17 15:02:03 +0300

    Многопоточность Часть 4. Синхронизация методов и мониторы

--- src/Multithreading/Bank.java
+++ src/Multithreading/Bank.java
@@ -1,18 +1,58 @@
 package Multithreading;
 
 public class Bank {
-    public int money = 10_000;
 
-    public void takeMoney(String name, int sum){
-        System.out.println(name + " подошел к банкомату");
+    public int money = 100;
 
+    public void takeMoney(String name, int sum) {
+
+        synchronized (this){
+            System.out.println(name + " подошел к банкомату");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            if (money < sum) {
+                System.out.println("Денег в банкомате не достаточно");
+            } else {
+                money -= sum;
+                System.out.println(name + " снял " + sum);
+            }
+            System.out.println("В банкомате осталось " + money);
+        }
     }
+}
 
-    public void takeMoney2(String name, int sum){
+class Main{
 
-    }
+    public static void main(String[] args) {
+        Bank bank = new Bank();
 
-    public void takeMoney3(String name, int sum){
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                bank.takeMoney("Вася", 100);
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                bank.takeMoney("Антон", 100);
+            }
+        });
+
+        Thread thread3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                bank.takeMoney("Игорь", 10_000);
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
 
     }
 }
